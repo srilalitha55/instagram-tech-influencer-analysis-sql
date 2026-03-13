eac37e436442195a2cca21b9f795f29c0490bade
G R S S SRI LALITHA <lalithagoparaju77@gmail.com>
2026-03-13 18:22:17 +0530

    Delete QUERY_07.sql

--- QUERY_07.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  post_type,
-  SUM(reach) AS total_reach,
-  ROUND(100 * SUM(reach) / SUM(SUM(reach)) OVER (), 2) AS reach_percentage
-FROM gdb0120.fact_content
-GROUP BY post_type
-ORDER BY total_reach DESC;
